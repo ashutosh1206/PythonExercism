aab482630babd758e75d55e17c0e697759a324f9
ashutosh1206 <ashutosh.ahelleya@gmail.com>
2016-10-19 17:13:32 +0530

    Hamming

---
+++ hamming/hamminga.py
@@ -0,0 +1,8 @@
+str1= raw_input("Enter one strang of DNA: ")
+str2= raw_input("Enter another strang of DNA: ")
+i=0,count=0
+while(str1[i]!='\0' && str2[i]!='\0'):
+    if(str1[i]==str2[i]):
+        count=count+1
+    i=i+1
+print("The hamming difference between the two DNA strangs is %d",count)
