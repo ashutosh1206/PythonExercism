7c1de488871e95e7a8e33d5b7e1b4346d0d78c93
ashutosh1206 <ashutosh.ahelleya@gmail.com>
2016-10-19 17:20:05 +0530

    Hello World

--- hamming/hamminga.py
+++ hamming/hamminga.py
@@ -1,7 +1,7 @@
 str1= raw_input("Enter one strang of DNA: ")
 str2= raw_input("Enter another strang of DNA: ")
 i=0,count=0
-while(str1[i]!='\0' && str2[i]!='\0'):
+while(str1[i]!='\0' and str2[i]!='\0'):
     if(str1[i]==str2[i]):
         count=count+1
     i=i+1
