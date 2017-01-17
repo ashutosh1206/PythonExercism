bc66fab308da8a32362f1d51b7762402de0c6436
ashutosh1206 <ashutosh.ahelleya@gmail.com>
2017-01-17 21:40:52 +0530

    Modified Crypto-Square

--- crypto-square/crypto_square.py
+++ crypto-square/crypto_square.py
@@ -1,3 +1,7 @@
+from __future__ import print_function
+
+import sys
+#determining the number of rows and columns required for making the rectangle
 def detmn(num):
     for i in xrange(2,num/2+1):
         if i*(i+1) >= num:
@@ -5,14 +9,42 @@ def detmn(num):
             break
 
 
-def main():
-    str1 = raw_input("Enter the string to encoded as per crypto-square: ")
-    str1 = str1.replace(' ','')
-    print str1
-    num = len(str1)
-    print num
-    m,n = detmn(num)
-    print m,n
+#inputting the string and remove spaces
+str1 = raw_input("Enter the string to encoded as per crypto-square: ")
+for i in str1:
+    if ((ord(i)<97 and ord(i)>90) or ord(i)<65 or ord(i)>122):
+        str1 = str1.replace(i,'')
+str1 = str1.replace(' ','')
+
+
+#print the original string and number of rows and columns
+print(str1)
+num = len(str1)
+print("length: ",num)
+m,n = detmn(num)
+print("Number of rows: ",m)
+print("Number of columns: ",n)
+    
+
+#printing the rectangle
+for i in xrange(len(str1)):
+        if (i+1) % n == 0:
+            print(str1[i])
+        else:
+            print(str1[i],end="")
+print("")
+
+
+#encoding using the rectangle
+print("The encoded message is: ")
+for i in xrange(n):
+    j = i
+    while j<len(str1):
+        print(str1[j],end="")
+        j += n
+
+print("")
+
     
     
 
