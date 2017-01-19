c5020ea1cd561e8c531aba7cc39d725257e3dba2
ashutosh1206 <ashutosh.ahelleya@gmail.com>
2017-01-19 19:57:49 +0530

    Added Decoder function

--- rail-fence-cipher/rail_fence_cipher.py
+++ rail-fence-cipher/rail_fence_cipher.py
@@ -2,7 +2,6 @@ from __future__ import print_function
 import sys
 
 
-
 def encode(str1):
     str2 = ""
     for i in xrange(3):
@@ -24,13 +23,31 @@ def encode(str1):
         print("")
     return str2
 
-#def decode(str1):
-    #for i in xrange(
+def decode(str1):
+    rownum = 1
+    pos = 0
+    for rownum in xrange(1,4):
+        temp = 0
+        if rownum != 1:
+            if rownum == 2:
+                print(".",end=" ")
+                temp += 1
+            else:
+                print(". .",end=" ")
+                temp += 2
+        i = temp
+        while i < len(str1):
+            print(str1[pos],end=" ")
+            pos += 1
+            if rownum == 2:
+                i += 2
+                print(".",end=" ")
+            else:
+                i += 4
+                print(". . .",end=" ")
+        print("")
+            
     
-    
-    
-    
-       
 print("1. Encode")
 print("2. Decode")
 c =  input("Enter Choice: ")
@@ -42,8 +59,7 @@ if c ==1:
 elif c == 2:
     str1 = raw_input("Enter the string to be decoded: ")
     str1 = str1.replace(' ','')
-    decoded = decode(str1)
-    print("The decoded form of the string is: ",decoded)
+    decode(str1)
 else:
     raise AssertionError
 
